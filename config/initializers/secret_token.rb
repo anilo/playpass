eeccbb7f7b717b2f4191d26a6ddbd6b02b038662
Anil Punjabi <anilo@hotmail.com>
2011-12-04 22:35:05 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playpass::Application.config.secret_token = 'f701ecc7facd90b9e1e91a01f2a81f71b4ea6b492c9431fb1004d00ca6279050a49b1d5cbc992d7d703bffdae109d6d6857e6e9137e0305eb62f24d31fca6481'
